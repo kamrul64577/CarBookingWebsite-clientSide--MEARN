88db227c8204cd94d4c7eb5dc9446d8772cb9793
Kamrul <k.islam64577@gmail.com>
2021-11-26 20:08:52 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Getting Started with Create React App
+
+This is My cars website [Cars Home](https://assignment12-2d5f4.firebaseapp.com/).
+
+* Created with react, node, firebase and mongodb
+* authenication for admin and user
+* anyone can buy cars
+* any user can give review
+* admin can manage cars and orders
---
+++ src/hooks/useFirebas.js
@@ -0,0 +1,155 @@
+import { useEffect, useState } from "react";
+import initializeFirebase from "../Pages/LogIn/Firebase/firebase.init";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, updateProfile, getIdToken, signOut } from "firebase/auth";
+
+
+// Initialize firebase app
+initializeFirebase();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [authError, setAuthError] = useState('');
+    const [admin, setAdmin] = useState(false);
+    const [token, setToken] = useState('');
+    const auth = getAuth();
+
+    const googleProvider = new GoogleAuthProvider();
+
+    // User Registration
+    const registerUser = (email, password, name, history) => {
+        setIsLoading(true)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setAuthError('');
+                const newUser = { email, displayName: name }
+                setUser(newUser);
+
+                // Save user to the database
+                saveUser(email, name, 'POST');
+
+                // send name to firebase after creation
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                }).then(() => {
+                    // Profile updated!
+                    // ...
+                }).catch((error) => {
+                    // An error occurred
+                    // ...
+                });
+
+                history.replace('/');
+            })
+            .catch((error) => {
+
+                setAuthError(error.message);
+
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+
+    // User Login
+    const loginUser = (email, password, location, history) => {
+        setIsLoading(true)
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const redirect_uri = location.state?.from || '/home';
+                history.replace(redirect_uri);
+                setAuthError('');
+            })
+            .catch((error) => {
+
+                setAuthError(error.message)
+            })
+            .finally(() => setIsLoading(false));
+    }
+
+    const siginWithGoogle = (location, history) => {
+        setIsLoading(true)
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+
+                const user = result.user;
+                saveUser(user.email, user.displayName, 'PUT');
+
+                const destination = location?.history?.from || '/appointment';
+                history.replace(destination);
+                setAuthError('');
+
+            }).catch((error) => {
+
+                setAuthError(error.message)
+
+            }).finally(() => setIsLoading(false));
+    }
+
+    // Observe user state
+
+    useEffect(() => {
+        const unsubcribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+                getIdToken(user)
+                    .then(idToken => {
+                        setToken(idToken);
+                    })
+            } else {
+                setUser({})
+            }
+            setIsLoading(false);
+        });
+
+        return () => unsubcribed;
+
+    }, [])
+
+
+    const saveUser = (email, displayName, method) => {
+        const user = { email, displayName }
+        fetch('https://desolate-beyond-74023.herokuapp.com/users', {
+            method: method,
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then()
+    }
+
+    useEffect(() => {
+        fetch(`https://desolate-beyond-74023.herokuapp.com/users/${user.email}`)
+            .then(res => res.json())
+            .then(data => setAdmin(data.admin))
+    }, [user.email])
+
+
+    // User Logout
+    const logOut = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+
+        })
+            .finally(() => setIsLoading(false));
+    }
+
+    return {
+        user,
+        authError,
+        isLoading,
+        siginWithGoogle,
+        registerUser,
+        loginUser,
+        admin,
+        token,
+        logOut,
+
+    }
+
+}
+
+export default useFirebase;
